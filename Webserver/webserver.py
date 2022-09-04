891102df09013f5e23d1871907583a35541cfbe3
Aarav Arora <aaravarora.works@gmail.com>
2022-09-05 00:58:19 +0530

    Bug: Useless

--- Webserver/webserver.py
+++ Webserver/webserver.py
@@ -4,7 +4,6 @@ import sys
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
-sys.path.append("Drivogram")
 import asyncio
 import random
 from io import BytesIO
