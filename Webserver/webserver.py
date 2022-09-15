307e5faed92cc5a490f37c4ff7661fce1f3f7a19
Aarav Arora <aaravarora.works@gmail.com>
2022-09-15 18:30:27 +0530

    enhance: catching errors

--- Webserver/webserver.py
+++ Webserver/webserver.py
@@ -51,11 +51,14 @@ web.add_middleware(
 @web.on_event("startup")
 async def startup():
     async def client_start():
-        await app1.start()
-        await app2.start()
-        await app3.start()
-        await app4.start()
-        await idle()
+        try:
+            await app1.start()
+            await app2.start()
+            await app3.start()
+            await app4.start()
+            await idle()
+        except Exception as e:
+            print(e)
 
     asyncio.create_task(client_start())
 
@@ -64,79 +67,107 @@ async def startup():
 async def home(
     IN_FILE: UploadFile, X_API_KEY: APIKey = Depends(auth.apikey)
 ):
-    content = await IN_FILE.read()
-    b = BytesIO(content)
-    b.name = IN_FILE.filename
-    random_client = random.choice(choose)
-    await data_object.create_file_table(table_name="FileData")
-    key_file = data_key(type="FILE-", len=7)
-    doc = await random_client.send_document(
-        chat_id, b, force_document=True, caption=f"{key_file}"
-    )
-    await data_object.insert_file_data(
-        filename=IN_FILE.filename,
-        fileSize=str(doc.document.file_size),
-        MessageID=doc.id,
-        FileKey=key_file,
-        UserID=X_API_KEY,
-        Content=IN_FILE.content_type,
-        Time=doc.date,
-    )
-    return JSONResponse(
-        status_code=200,
-        content={
-            "msg": "file uploaded successfully",
-            "file_key": key_file,
-            "user": X_API_KEY,
-        },
-    )
+    try:
+        content = await IN_FILE.read()
+        b = BytesIO(content)
+        b.name = IN_FILE.filename
+        random_client = random.choice(choose)
+        await data_object.create_file_table(table_name="FileData")
+        key_file = data_key(type="FILE-", len=7)
+        doc = await random_client.send_document(
+            chat_id, b, force_document=True, caption=f"{key_file}"
+        )
+        await data_object.insert_file_data(
+            filename=IN_FILE.filename,
+            fileSize=str(doc.document.file_size),
+            MessageID=doc.id,
+            FileKey=key_file,
+            UserID=X_API_KEY,
+            Content=IN_FILE.content_type,
+            Time=doc.date,
+        )
+        return JSONResponse(
+            status_code=200,
+            content={
+                "msg": "file uploaded successfully",
+                "file_key": key_file,
+                "user": X_API_KEY,
+            },
+        )
+    except Exception as e:
+        print(e)
+        return JSONResponse(
+            status_code=500,
+            content={"error": "some server error, please try later!"},
+        )
 
 
 @web.get("/api/signup")
 async def data(NAME: str | None = Header(default=None)):
-    await data_object.create_user_table("UserData")
-    if NAME == None or NAME == "":
-        raise HTTPException(
-            status_code=422,
-            detail="missing parameter 'NAME',provide a name",
+    try:
+        await data_object.create_user_table("UserData")
+        if NAME == None or NAME == "":
+            raise HTTPException(
+                status_code=422,
+                detail="missing parameter 'NAME',provide a name",
+            )
+        return JSONResponse(
+            {"X-API-KEY": (await data_object.add_user(NAME))},
+            status_code=200,
         )
-    return JSONResponse(
-        {"X-API-KEY": (await data_object.add_user(NAME))},
-        status_code=200,
-    )
+    except Exception as e:
+        print(e)
+        return JSONResponse(
+            status_code=500,
+            content={"error": "some server error, please try later!"},
+        )
 
 
 @web.get("/api/logincheck")
 async def login(X_API_KEY: str | None = Header(default=None)):
-    if X_API_KEY == None:
-        raise HTTPException(
-            status_code=422,
-            detail="NO X-API KEY PROVIDED UNABLE TO PROCEED",
+    try:
+        if X_API_KEY == None:
+            raise HTTPException(
+                status_code=422,
+                detail="NO X-API KEY PROVIDED UNABLE TO PROCEED",
+            )
+        x = await data_object.login_check(X_API_KEY)
+        if x == None:
+            raise HTTPException(
+                status_code=401,
+                detail="Unauthorized Login, Please signup",
+            )
+        return JSONResponse(
+            status_code=200,
+            content={
+                "login": True,
+                "user": x,
+            },
         )
-    x = await data_object.login_check(X_API_KEY)
-    if x == None:
-        raise HTTPException(
-            status_code=401,
-            detail="Unauthorized Login, Please signup",
+    except Exception as e:
+        print(e)
+        return JSONResponse(
+            status_code=500,
+            content={"error": "some server error, please try later!"},
         )
-    return JSONResponse(
-        status_code=200,
-        content={
-            "login": True,
-            "user": x,
-        },
-    )
 
 
 @web.get("/api/uploads")
 async def uploads(X_API_KEY: APIKey = Depends(auth.apikey)):
-    return JSONResponse(
-        status_code=200,
-        content={
-            "User": X_API_KEY,
-            "Uploads": (await data_object.get_uploads(X_API_KEY)),
-        },
-    )
+    try:
+        return JSONResponse(
+            status_code=200,
+            content={
+                "User": X_API_KEY,
+                "Uploads": (await data_object.get_uploads(X_API_KEY)),
+            },
+        )
+    except Exception as e:
+        print(e)
+        return JSONResponse(
+            status_code=500,
+            content={"error": "some server error, please try later!"},
+        )
 
 
 @web.delete("/api/delete")
@@ -144,17 +175,26 @@ async def delete(
     FILE_KEY: str | None = Header(default=None),
     X_API_KEY: APIKey = Depends(auth.apikey),
 ):
-    name = await data_object.deleteFile(FILE_KEY, X_API_KEY)
-    if name == None:
-        raise HTTPException(status_code=404, detail="File Not Found")
-    return JSONResponse(
-        status_code=200,
-        content={
-            "user": X_API_KEY,
-            "file": name,
-            "message": "Deleted the file successfully",
-        },
-    )
+    try:
+        name = await data_object.deleteFile(FILE_KEY, X_API_KEY)
+        if name == None:
+            raise HTTPException(
+                status_code=404, detail="File Not Found"
+            )
+        return JSONResponse(
+            status_code=200,
+            content={
+                "user": X_API_KEY,
+                "file": name,
+                "message": "Deleted the file successfully",
+            },
+        )
+    except Exception as e:
+        print(e)
+        return JSONResponse(
+            status_code=500,
+            content={"error": "some server error, please try later!"},
+        )
 
 
 @web.get("/api/download")
@@ -162,34 +202,45 @@ async def download(
     FILE_KEY: str | None = Header(default=None),
     X_API_KEY: APIKey = Depends(auth.apikey),
 ):
-    if FILE_KEY == None or FILE_KEY == "":
-        raise HTTPException(
-            status_code=404, detail="Invalid file Key"
+    try:
+        if FILE_KEY == None or FILE_KEY == "":
+            raise HTTPException(
+                status_code=404, detail="Invalid file Key"
+            )
+        message_id = await data_object.getFile(
+            file_key=FILE_KEY, User_id=X_API_KEY
         )
-    message_id = await data_object.getFile(
-        file_key=FILE_KEY, User_id=X_API_KEY
-    )
-    if message_id == None:
-        raise HTTPException(status_code=404, detail="File Not Found")
-    else:
-        random_client = random.choice(choose)
-        msg = await random_client.get_messages(chat_id, message_id)
-        file_id = msg.document.file_id
-        file_size = msg.document.file_size
-        file_name = msg.document.file_name
-        file_content = msg.document.mime_type
-        stream_data = chunk_stream(
-            client=random_client, fileID=file_id
+        if message_id == None:
+            raise HTTPException(
+                status_code=404, detail="File Not Found"
+            )
+        else:
+            random_client = random.choice(choose)
+            msg = await random_client.get_messages(
+                chat_id, message_id
+            )
+            file_id = msg.document.file_id
+            file_size = msg.document.file_size
+            file_name = msg.document.file_name
+            file_content = msg.document.mime_type
+            stream_data = chunk_stream(
+                client=random_client, fileID=file_id
+            )
+        return StreamingResponse(
+            stream_data,
+            status_code=200,
+            media_type=file_content,
+            headers={
+                "content-length": str(file_size),
+                "X-FILE-NAME": file_name,
+            },
         )
-    return StreamingResponse(
-        stream_data,
-        status_code=200,
-        media_type=file_content,
-        headers={
-            "content-length": str(file_size),
-            "X-FILE-NAME": file_name,
-        },
-    )
+    except Exception as e:
+        print(e)
+        return JSONResponse(
+            status_code=500,
+            content={"error": "some server error, please try later!"},
+        )
 
 
 class Share(BaseModel):
@@ -201,66 +252,89 @@ class Share(BaseModel):
 @web.post("/api/share")
 async def sharable(
     share: Share, X_API_KEY: APIKey = Depends(auth.apikey)
-):  # )
-    new_time = int(time.time()) + share.exp * 3600
-    await data_object.create_share_table()
-    encrypt_client = Fernet(config.jwt_secret)
-    encrypted = encrypt_client.encrypt(
-        str(share.__dict__).encode("utf-8")
-    ).decode("utf-8")
-    random = data_key("", len=8)
-    await data_object.share_data_add(
-        short=random, token=encrypted, userid=X_API_KEY, time=new_time
-    )
-    return JSONResponse(
-        status_code=200,
-        content={
-            "link": f"{config.domain_name}/share/?token={random}",
-            "time": share.exp,
-        },
-    )
+):
+    try:
+        new_time = int(time.time()) + share.exp * 3600
+        await data_object.create_share_table()
+        encrypt_client = Fernet(config.jwt_secret)
+        encrypted = encrypt_client.encrypt(
+            str(share.__dict__).encode("utf-8")
+        ).decode("utf-8")
+        random = data_key("", len=8)
+        await data_object.share_data_add(
+            short=random,
+            token=encrypted,
+            userid=X_API_KEY,
+            time=new_time,
+        )
+        return JSONResponse(
+            status_code=200,
+            content={
+                "link": f"{config.domain_name}/share/?token={random}",
+                "time": share.exp,
+            },
+        )
+    except Exception as e:
+        print(e)
+        return JSONResponse(
+            status_code=500,
+            content={"error": "some server error, please try later!"},
+        )
 
 
 @web.get("/share/")
 async def share(token: str):
-    current_time = int(time.time())
-    enc_token, time_token = await data_object.share_data_search(
-        shorten=token
-    )
-    if current_time > int(time_token):
-        raise HTTPException(
-            status_code=503, detail="The sharing session has expired"
+    try:
+        current_time = int(time.time())
+        enc_token, time_token = await data_object.share_data_search(
+            shorten=token
         )
-    decrypt_client = Fernet(config.jwt_secret)
-    decrypted = (
-        decrypt_client.decrypt(token=enc_token.encode("utf-8"))
-        .decode("utf-8")
-        .replace("'", '"')
-    )
-    final_data = json.loads(decrypted)
-    message_id = await data_object.getFile(
-        file_key=final_data["filekey"], User_id=final_data["userkey"]
-    )
-    if message_id == None:
-        raise HTTPException(status_code=404, detail="Not Found")
-    else:
-        random_client = random.choice(choose)
-        msg = await random_client.get_messages(chat_id, message_id)
-        file_id = msg.document.file_id
-        file_size = msg.document.file_size
-        file_name = msg.document.file_name
-        file_content = msg.document.mime_type
-        stream_data = chunk_stream(
-            client=random_client, fileID=file_id
+        if current_time > int(time_token):
+            raise HTTPException(
+                status_code=503,
+                detail="The sharing session has expired",
+            )
+        decrypt_client = Fernet(config.jwt_secret)
+        decrypted = (
+            decrypt_client.decrypt(token=enc_token.encode("utf-8"))
+            .decode("utf-8")
+            .replace("'", '"')
         )
-        return StreamingResponse(
-            stream_data,
-            status_code=200,
-            media_type=file_content,
-            headers={
-                "content-length": str(file_size),
-                "X-FILE-NAME": file_name,
-            },
+        final_data = json.loads(decrypted)
+        message_id = await data_object.getFile(
+            file_key=final_data["filekey"],
+            User_id=final_data["userkey"],
+        )
+        if message_id == None:
+            raise HTTPException(
+                status_code=404, detail="File Not Found"
+            )
+        else:
+            random_client = random.choice(choose)
+            msg = await random_client.get_messages(
+                chat_id, message_id
+            )
+            file_id = msg.document.file_id
+            file_size = msg.document.file_size
+            file_name = msg.document.file_name
+            file_content = msg.document.mime_type
+            stream_data = chunk_stream(
+                client=random_client, fileID=file_id
+            )
+            return StreamingResponse(
+                stream_data,
+                status_code=200,
+                media_type=file_content,
+                headers={
+                    "content-length": str(file_size),
+                    "X-FILE-NAME": file_name,
+                },
+            )
+    except Exception as e:
+        print(e)
+        return JSONResponse(
+            status_code=500,
+            content={"error": "some server error, please try later!"},
         )
 
 
